e35f9408ca34a044515e0e0fc3cd35dba1ba5bb2
Снежана <149336836+Snowflake-QA@users.noreply.github.com>
2023-11-10 01:17:23 +0300

    Update Main.java

--- src/main/java/com/gmail/ganna/Main.java
+++ src/main/java/com/gmail/ganna/Main.java
@@ -2,6 +2,8 @@ package com.gmail.ganna;
 
 public class Main {
     public static void main(String[] args) {
+         System.out.println("Hello world!");
+        
         System.out.println("Oh. Hello Snowflake!!!!!!!!");
     }
 }
