6f699ff62d4fe990b3ded1262545fba457ac3dc9
ganzha.s <ganna8989@gmail.com>
2023-11-09 21:22:09 +0300

    commit for change2

--- src/main/java/com/gmail/ganna/Main.java
+++ src/main/java/com/gmail/ganna/Main.java
@@ -4,4 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    private static getMassedge() { return "Hello Snowlake_2!"; }
 }
