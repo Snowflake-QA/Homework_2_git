034e2ef16332b6dd99080c5866886f16e409d3e7
ganzha.s <ganna8989@gmail.com>
2023-11-10 01:53:56 +0300

    55

--- src/main/java/com/gmail/ganna/Main.java
+++ src/main/java/com/gmail/ganna/Main.java
@@ -1,6 +1,6 @@
 package com.gmail.ganna;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! 55");
     }
 }
